f5fa16fa81caa4b3b97acc75058cd5a1ba0d29f8
Pimpsten <flinds24@student.aau.dk>
2026-05-28 15:47:53 +0200

    Update Workshop.ino

--- Workshop.ino
+++ Workshop.ino
@@ -1,24 +1,45 @@
 #include <krnl.h>
-struct k_t *pTask1, *pTask2, *pTask3, *pTask4, *pTask5, *pTask6;  // task
-int maxSpeed, currentSpeed, newSpeed;
-float desiredSpeed;
+#define STK 150
+struct speedData {
+  float rmp, time;
+};
 
-unsigned char taskStak1[STK],taskStak2[STK],taskStak3[STK],taskStak4[STK],taskStak5[STK],taskStak6[STK];
+struct allInfo {
+  float time, currentSpeed, desiredSpeed, maxSpeed;
+};
+
+speedData speedBuffer[2];
+allInfo infoBuffer[2];
+int maxSpeedBuffer[2];
+
+
+struct k_msg_t *Queue_Ctrl, *Queue_ReadSpd, *Queue_Cmot;          // message queues
+struct k_t *pTask1, *pTask2, *pTask3, *pTask4, *pTask5, *pTask6;  // task
+struct k_t *Timed_sem1, *CommIn_sem2, *ReadMtr_sem3;              // semaphores
+
+unsigned char taskStak1[STK], taskStak2[STK], taskStak3[STK], taskStak4[STK], taskStak5[STK], taskStak6[STK];
 
 void setup() {
   Serial.begin(115200);
-  k_init(6, /*to be chosen*/, /*to be chosen*/);
+  k_init(6, 3, 3);
   pTask1 = k_crt_task(CommIn, 10, taskStak1, 150);       // Seriel protocol
   pTask2 = k_crt_task(TaskCtrl, 10, taskStak2, 150);     // Processer nedenstående variabler
   pTask3 = k_crt_task(ReadMeter, 10, taskStak3, 150);    // Læser potentiometer
   pTask4 = k_crt_task(ReadSpeed, 10, taskStak4, 150);    // Læser current speed
   pTask5 = k_crt_task(ChangeSpeed, 10, taskStak5, 150);  // Ændrer hastighed
-  pTask6 = k_crt_task(CommOut,10,taskStak6,150);         // Printer værdierne
+  pTask6 = k_crt_task(CommOut, 10, taskStak6, 150);
 
+  Queue_Ctrl = k_crt_send_Q(2, sizeof(int), maxSpeedBuffer);
+  Queue_ReadSpd = k_crt_send_Q(2, sizeof(speedData), speedBuffer);
+  Queue_Cmot = k_crt_send_Q(2, sizeof(allInfo), InfoBuffer);
 
+  Timed_Sem1 = k_crt_sem(0, 1);
+  CommIn_sem2 = k_crt_sem(0, 1);
+  ReadMtr_sem3 = k_crt_sem(0, 1);
 
   k_start();
-  while(1);
+  while (1)
+    ;
 }
 
 void CommIn(){
